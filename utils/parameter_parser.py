b62a8bd7ddd5779759bfc97394a08c133498ef90
Weilin Xu <mzweilin@gmail.com>
2017-11-21 22:13:20 -0500

    Updated the param parser.

--- utils/parameter_parser.py
+++ utils/parameter_parser.py
@@ -3,7 +3,8 @@ install_aliases()
 from urllib import parse as urlparse
 
 def parse_params(params_str):
-    assert '?' in params_str
+    if '?' not in params_str:
+        return params_str, {}
     subject, params_str = params_str.split('?')
     params = urlparse.parse_qs(params_str)
     params = dict( (k, v if len(v)>1 else v[0] ) for k,v in params.items())
