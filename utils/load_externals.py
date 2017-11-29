3f5ced5e49b9522fe7798ff6d4373f3bc7ecd81b
Weilin Xu <mzweilin@gmail.com>
2017-11-29 12:02:16 -0500

    Added MagNet in externals.

--- utils/load_externals.py
+++ utils/load_externals.py
@@ -6,6 +6,7 @@ external_libs = {'Cleverhans': "externals/cleverhans",
                  "MobileNets": "externals/MobileNetworks",
                  "Deepfool/Universal": "externals/universal/python",
                  "DenseNet": "externals/titu1994/DenseNet",
+                 "MagNet": "externals/MagNet",
                  }
 
 project_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -37,8 +38,11 @@ for lib_name, lib_path in external_libs.items():
         lib_token_fpath = os.path.join(lib_path, '__init__.py')
         if not os.path.isfile(lib_token_fpath):
             open(lib_token_fpath, 'a').close()
+
+    if lib_name in ['MagNet']:
+        lib_token_fpath = os.path.join(lib_path, '__init__.py')
+        if not os.path.isfile(lib_token_fpath):
+            open(lib_token_fpath, 'a').close()
     
     sys.path.append(lib_path)
     print("Located %s" % lib_name)
-
-# print (sys.path)
