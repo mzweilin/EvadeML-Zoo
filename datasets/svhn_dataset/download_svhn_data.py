2f1f05e3cbe10cd630842eef93a9a2258103b17a
fqdhlyc <33609027+fqdhlyc@users.noreply.github.com>
2018-12-19 17:34:10 -0500

    Update download_svhn_data.py

--- datasets/svhn_dataset/download_svhn_data.py
+++ datasets/svhn_dataset/download_svhn_data.py
@@ -1,11 +1,15 @@
 import urllib2
-import os
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append('datasets/svhn_dataset/')
 
 filename = ['test_32x32.mat','train_32x32.mat']
 addr = ['http://ufldl.stanford.edu/housenumbers/test_32x32.mat','http://ufldl.stanford.edu/housenumbers/train_32x32.mat']
 
 for i in range(2):
-	if not os.path.exists(filename[i]):
-		output = open(filename[i],'w')
+	f = os.path.join('datasets/svhn_dataset',filename[i])
+	if not os.path.exists(f):
+		output = open(f,'w')
 		output.write(urllib2.urlopen(addr[i]).read())
 		output.close()
